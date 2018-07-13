b598c43cb11a9d56a60d651dec5a7114e1a54511
Aleksey <permenev.alex@ya.ru>
2018-07-13 09:20:59 +0400

    commit

---
+++ migrates.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+python manage.py makemigrations store
+python manage.py migrate
--- store/models.py
+++ store/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=128)
+    code = models.AutoField(primary_key=True)
+    price = models.IntegerField()
+    sale = models.BooleanField(blank=True, null=True)
+    sale_price = models.IntegerField(blank=True, null=True)
+    description = models.TextField()
+    characteristics = models.TextField()
+    country = models.CharField(max_length=16)
+
+
+class Comment(models.Model):
+    author = models.CharField(max_length=16)
+    date_created = models.DateTimeField()
+    text = models.TextField(max_length=1024)
+    product_code = models.ForeignKey(Product, models.CASCADE)
+    id = models.AutoField(primary_key=True)
+    likes = models.IntegerField(blank=True, null=True)
+    dislikes = models.IntegerField(blank=True, null=True)
+    reply_rating = models.IntegerField(blank=True, null=True)
+    reply = models.IntegerField(blank=True, null=True)
+
+
+class Rating(Comment):
+    advantages = models.TextField(max_length=256, blank=True, null=True)
+    disadvantages = models.TextField(max_length=256, blank=True, null=True)
