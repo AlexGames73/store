921a2cafd7c00ee35c37f1f752aa03b414aefeec
AlexGames73 <permenev.alex@ya.ru>
2018-07-13 10:19:31 +0400

    commit

--- store/views.py
+++ store/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Product
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'store/index.html')
